012e6689228968564085c757195a0091eaf89e50
Diogo S. Lourenço <114771411+diogolourenco100@users.noreply.github.com>
2024-04-19 17:35:04 -0300

    Update fizzbuzz.py

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -1,14 +1,9 @@
-n = 50
+n = int(input('Enter a int number: '))
 
-for i in range(1, n + 1):
+for i in range(1, n+1):
     if i % 2 == 0 and i % 3 == 0:
-        print(f"{i} => FizzBuzz")
-
+        print(f'{i} -> FizzBuzz')
     elif i % 2 == 0:
-        print(f"{i} => Fizz")
-
+        print(f'{i} -> Fizz')
     elif i % 3 == 0:
-        print(f"{i} => Buzz")
-        
-    else:
-        print(i)
+        print(f'{i} -> Buzz')
